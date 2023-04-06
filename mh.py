952e6a12035be4aa9591d287dcddb52efe5eb2b9
HansGiesen <32932034+HansGiesen@users.noreply.github.com>
2023-04-06 16:27:48 -0400

    Update mh.py
    * Fixed issue that I accidentally called the car a goat.
    * Disabled all debug output.

--- mh.py
+++ mh.py
@@ -13,6 +13,7 @@ Note that this implementation focuses on readability rather than speed.
 import argparse
 import random
 
+DEBUG = False
 DOOR_CNT = 3
 
 parser = argparse.ArgumentParser()
@@ -28,28 +29,35 @@ if __name__ == '__main__':
     doors = list(range(1, DOOR_CNT + 1))
     wins = 0
     for play in range(args.play_cnt):
-        goat_door = random.choice(doors)
-        print(f'The goat is behind door {goat_door}.')
+        car_door = random.choice(doors)
+        if DEBUG:
+            print(f'The car is behind door {car_door}.')
 
         choice = random.choice(doors)
-        print(f'The player chooses door {choice}.')
+        if DEBUG:
+            print(f'The player chooses door {choice}.')
 
-        options = set(doors) - set([goat_door, choice])
+        options = set(doors) - set([car_door, choice])
         open_door = random.choice(list(options))
-        print(f'The host opens door {open_door}.')
+        if DEBUG:
+            print(f'The host opens door {open_door}.')
 
         switch = random.uniform(0, 1)
         if switch < args.switch_prob:
             options = set(doors) - set([open_door, choice])
             choice = random.choice(list(options))
-            print('The player switches door.')
+            if DEBUG:
+                print('The player switches door.')
         else:
-            print('The player does not switch door.')
+            if DEBUG:
+                print('The player does not switch door.')
 
-        if choice == goat_door:
-            print('The player won.\n')
+        if choice == car_door:
+            if DEBUG:
+                print('The player won.\n')
             wins += 1
         else:
-            print('The player lost.\n')
+            if DEBUG:
+                print('The player lost.\n')
 
     print(f'Win percentage: {wins / args.play_cnt}')
